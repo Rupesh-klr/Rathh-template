97585dc6ec5c0eb0d737bf7a36666843732b6ab9
Yuvarajkurri <kurriyuvaraj13@gmail.com>
2026-02-23 18:48:13 +0530

    NotFOUND PAGE ADDED

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Home from './pages/home';
 import ContactUs from './pages/ContactUs';
 import Dyna from './pages/Dyna';
- 
+import NotFound from './pages/NotFound';
 import { Routes, Route } from 'react-router-dom';
  
 
